ddc5ae3dad9decf61f6a323accc6b7156c00452e
TeeSeal <teesealz@gmail.com>
2019-02-04 00:58:58 +0200

    Stack everything in one html file cause why the fuck not

--- index.js
+++ index.js
@@ -4,11 +4,5 @@ const port = process.env.PORT || 4000
 const { join } = require('path')
 
 app.use(express.static('assets'))
-app.use(express.static('js'))
-app.use(express.static('css'))
-
-app.get('*', (_, res) => {
-  res.sendFile(join(__dirname, 'html', 'index.html'))
-})
-
+app.get('*', (_, res) => res.sendFile(join(__dirname, 'index.html')))
 app.listen(port, () => console.log(`http://localhost:${port}`))
