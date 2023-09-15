6c083581a02bb3d068ddffe35b062a5b85828a23
Alex Hincu <alex@teeseal.dev>
2023-09-15 15:05:29 +0300

    Transform to static HTML page

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,2 @@
-FROM node:alpine
-
-WORKDIR /usr/src/app
-
-COPY package.json yarn.lock ./
-RUN yarn install --production
-
-ENV PORT 80
-EXPOSE 80
-
-COPY . .
-CMD ["yarn", "start"]
+FROM nginx:alpine
+COPY ./dist /usr/share/nginx/html
--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-const { join } = require('path')
-
-app.use(express.static('assets'))
-app.get('*', (_, res) => res.sendFile(join(__dirname, 'index.html')))
-app.listen(port, () => console.log(`http://localhost:${port}`))
